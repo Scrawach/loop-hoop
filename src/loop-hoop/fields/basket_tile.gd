c79200b30482a816de1f95717be31b2a5598fefb
scrawach <scrawach@gmail.com>
2025-08-02 19:48:04 +0300

    feat: update field visuals

--- src/loop-hoop/fields/basket_tile.gd
+++ src/loop-hoop/fields/basket_tile.gd
@@ -1,7 +1,33 @@
+@tool
 class_name BasketTile
 extends Node3D
 
 @export var add_money_label: PackedScene
+
+@export var has_hoop_circe: bool:
+	set(value):
+		has_hoop_circe = value
+		$"tile-base/tile-hoop-circle".visible = value
+
+@export_enum("none", "horizontal_forward", "horizontal_back") var horizontal_line: int:
+	set(value):
+		horizontal_line = value
+		$"tile-base/tile-horizontal-line".visible = value == 1
+		$"tile-base/tile-horizontal-line-2".visible = value == 2
+
+@export_enum("none", "vertical_left", "vertical_right") var vertical_line: int:
+	set(value):
+		vertical_line = value
+		$"tile-base/tile-vertical-line".visible = value == 1
+		$"tile-base/tile-vertical-line-2".visible = value == 2
+
+@export_enum("none", "0", "90", "180", "270") var angle_line: int:
+	set(value):
+		angle_line = value
+		$"tile-base/tile-angle-line-0".visible = value == 1
+		$"tile-base/tile-angle-line-90".visible = value == 2
+		$"tile-base/tile-angle-line-180".visible = value == 3
+		$"tile-base/tile-angle-line-270".visible = value == 4
 
 var row_index: int
 
