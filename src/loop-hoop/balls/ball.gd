7960f4b86aaf2adce87d39b61782a47f7f4be659
scrawach <scrawach@gmail.com>
2025-08-02 16:03:57 +0300

    feat: upload basketball glb model, use lerp instead of calculation direction

--- src/loop-hoop/balls/ball.gd
+++ src/loop-hoop/balls/ball.gd
@@ -1,43 +1,36 @@
 class_name Ball
 extends Node3D
 
+const MINIMAL_DISTANCE: float = 0.1
+
 @export var speed: float
 
 @export var current_tile: BasketTile
 @export var target_tile: BasketTile
 
 @export var field: BasketField
-@export var add_money_label: PackedScene
 
 var is_forward: bool = true
+var progress: float = 0
 
 func _process(delta: float) -> void:
 	if not current_tile or not target_tile:
 		return
 	
-	if not is_target_reached():
+	if progress < 1:
 		move(current_tile, target_tile, delta)
 	else:
+		progress = 0
 		take_next_target()
-		give_money()
 
 func move(from: BasketTile, to: BasketTile, delta: float) -> void:
-	var direction := (to.global_position - from.global_position).normalized()
-	var movement := direction * speed * delta
-	position += movement
-
-func is_target_reached() -> bool:
-	return (target_tile.global_position - global_position).length() < 0.2
+	progress += delta * speed
+	global_position = from.global_position.lerp(to.global_position, progress)
 
 func take_next_target() -> void:
 	if field.is_should_change_direction(target_tile):
 		is_forward = !is_forward
 	
 	current_tile = target_tile
+	current_tile.interact(self)
 	target_tile = field.get_next_tile(current_tile, is_forward)
-
-func give_money() -> void:
-	var instance := add_money_label.instantiate() as AddMoneyLabel
-	get_parent().add_child(instance)
-	instance.global_position = global_position + Vector3.UP
-	Game.wallet.add_money(1)
--- src/loop-hoop/fields/basket_tile.gd
+++ src/loop-hoop/fields/basket_tile.gd
@@ -1,4 +1,15 @@
 class_name BasketTile
 extends Node3D
 
+@export var add_money_label: PackedScene
+
 var row_index: int
+
+func interact(ball: Ball) -> void:
+	give_money()
+
+func give_money() -> void:
+	var instance := add_money_label.instantiate() as AddMoneyLabel
+	get_parent().add_child(instance)
+	instance.global_position = global_position
+	Game.wallet.add_money(1)
